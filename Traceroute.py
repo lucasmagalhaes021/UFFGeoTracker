3a348a38860f4c75fb71b7124ac6f0962907dd8a
Lucas Magalhães <lucasferreiramagalhaes@outlook.com>
2024-04-14 22:59:52 -0300

    Adição de uma interface pro sistema

--- Traceroute.py
+++ Traceroute.py
@@ -10,8 +10,9 @@ def traceroute(target, tracerouteResult):
     process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     CommandOutput, _ = process.communicate()
     CommandOutput = CommandOutput.decode('utf-8', 'ignore')
-    
     formattedIpList = re.findall(r'\d+\.\d+\.\d+\.\d+', CommandOutput)
+    if formattedIpList:
+        formattedIpList.pop(0) #Remove o IP Destino do cabeçalho, para não duplicar na tabela
     tracerouteResult.extend(formattedIpList)
 
 def SpinnerAnimation(event):
@@ -32,30 +33,36 @@ def getIpDetails(ip):
         return None
 
 def main():
-    address = input("Digite a URL: ")
-    tracerouteResult = []
-    doneEvent = threading.Event()
-    
-    tracerouteThread = threading.Thread(target=traceroute, args=(address, tracerouteResult))
-    loadingThread = threading.Thread(target=SpinnerAnimation, args=(doneEvent,))
-    
-    tracerouteThread.start()
-    loadingThread.start()
-    
-    tracerouteThread.join()
-    doneEvent.set()
-    loadingThread.join()
-    
-    ipMapperList = []
-    headers = ["IP Address", "Latitude", "Longitude", "Provedor de Internet", "Cidade", "País"]
-    for ip in tracerouteResult:
-        info = getIpDetails(ip)
-        if info and info['status'] == 'success':
-            ipData = [ip, info['lat'], info['lon'], info['isp'], info['city'], info['country']]
-            ipMapperList.append(ipData)
-    
-    # Formata e exibe os resultados em uma tabela bonita
-    print(tabulate(ipMapperList, headers=headers, tablefmt="grid"))
+    while True:
+        address = input("Digite a URL: ")
+        tracerouteResult = []
+        doneEvent = threading.Event()
+
+        tracerouteThread = threading.Thread(target=traceroute, args=(address, tracerouteResult))
+        loadingThread = threading.Thread(target=SpinnerAnimation, args=(doneEvent,))
+
+        tracerouteThread.start()
+        loadingThread.start()
+
+        tracerouteThread.join()
+        doneEvent.set()
+        loadingThread.join()
+
+        ipMapperList = []
+        headers = ["IP Address", "Latitude", "Longitude", "Provedor de Internet", "Cidade", "País"]
+        for ip in tracerouteResult:
+            info = getIpDetails(ip)
+            if info and info['status'] == 'success':
+                ipData = [ip, info['lat'], info['lon'], info['isp'], info['city'], info['country']]
+                ipMapperList.append(ipData)
+
+        # Formata e exibe os resultados em uma tabela bonita
+        print(tabulate(ipMapperList, headers=headers, tablefmt="grid"))
+
+        # Pergunta ao usuário se ele deseja fazer uma nova busca ou sair
+        user_choice = input("Digite 1 para fazer uma nova busca, ou 2 para sair: ")
+        if user_choice == "2":
+            break
 
 if __name__ == "__main__":
     main()
